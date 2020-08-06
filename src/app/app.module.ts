cec2403bb74f650f2da19a9ccae306b9edfaa8e7
Alayendecker <alcommits@gmail.com>
2020-08-06 15:31:14 -0500

    HandleUserInput

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PersonsComponent } from './persons/persons.component';
+import { PersonInputComponent } from './persons/person-input.component';
 
 @NgModule({
-  declarations: [AppComponent, PersonsComponent],
+  declarations: [AppComponent, PersonsComponent, PersonInputComponent],
   imports: [BrowserModule],
   providers: [],
   //only root app has to be registered in boostrap
