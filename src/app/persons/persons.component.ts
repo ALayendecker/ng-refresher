cf751c8cbf67849d43af847dfda3f9292f0915a0
Alayendecker <alcommits@gmail.com>
2020-08-20 11:16:29 -0500

    live updates to list with subscriptions

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,7 +1,7 @@
 //Input is a decorator that allows us to bind components
 // don't need a bindable input anymore if we're using services
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { PersonsService } from './persons.service';
 
 //dont forget to register component in app.module.ts
@@ -10,8 +10,9 @@ import { PersonsService } from './persons.service';
   selector: 'app-persons',
   templateUrl: './persons.component.html',
 })
-export class PersonsComponent implements OnInit {
+export class PersonsComponent implements OnInit, OnDestroy {
   personList: string[];
+  private personListSubs: Subscription;
   //long form instead of shorthand
   // private personService: PersonsService;
 
@@ -19,13 +20,23 @@ export class PersonsComponent implements OnInit {
   constructor(private personService: PersonsService) {
     //this.personList = personService.persons;
     // instead of using it like this we should be doing lifecycle hooks instead
-    this.personService = personService;
+    // this.personService = personService;
     // there is a shortcut using this but instead providing private when passing in personService in the constructor function
   }
 
   // this gets fired when our component is created essentially on component did mount
   ngOnInit() {
     this.personList = this.personService.persons;
+    this.personListSubs = this.personService.personsChanged.subscribe(
+      (persons) => {
+        this.personList = persons;
+      }
+    );
+  }
+
+  // we need destroy when working with subscribes and subjects in order to prevent memory leaks from hanging subscriptions
+  ngOnDestroy() {
+    this.personListSubs.unsubscribe();
   }
 
   // literally just passing the persons name from
--- src/app/persons/persons.service.ts
+++ src/app/persons/persons.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class PersonsService {
+  // when changing this to private it means it can only be used inside the service
+  // using Subject and injectible like this allows us to use subscriptions to push directly and live update
+  personsChanged = new Subject<string[]>();
+  // subject will hold string array data
   persons: string[] = ['Max', 'Ted', 'Anna'];
 
   addPerson(name: string) {
     this.persons.push(name);
+    //this persons Changed now emits an event everything addPerson is run
+    this.personsChanged.next(this.persons);
     console.log(this.persons);
   }
 
@@ -14,6 +21,8 @@ export class PersonsService {
     this.persons = this.persons.filter((person) => {
       return person !== name;
     });
+    //this persons Changed now emits an event everything removePerson is run
+    this.personsChanged.next(this.persons);
     console.log(this.persons);
   }
 }
